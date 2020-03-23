369f3a86bcde9445734b9e77afb9b4dd524da06b
Tharani2518 <49512294+Tharani2518@users.noreply.github.com>
2020-03-23 12:52:42 +0530

    Create Lowerupper.py

---
+++ Lowerupper.py
@@ -0,0 +1,7 @@
+ch = input("Please Enter Your  Character : ")
+if(ch.isupper()):
+    print("The Given Character ", ch, "is an Uppercase Alphabet")
+elif(ch.islower()):
+    print("The Given Character ", ch, "is a Lowercase Alphabet")
+else:
+    print("The Given Character ", ch, "is Not a Lower or Uppercase Alphabet")
